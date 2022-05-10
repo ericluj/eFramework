fb8f4394f2b0b070aa0cd1a4f51d6df6046d164c
ericluj <847751875@qq.com>
2022-05-10 18:32:35 +0800

    test

---
+++ test/rpc_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"context"
+	"testing"
+
+	"eFramework/rpc/sample"
+
+	"google.golang.org/grpc"
+)
+
+func TestRpc(t *testing.T) {
+	conn, err := grpc.Dial(":8000", grpc.WithInsecure())
+	if err != nil {
+		t.Errorf("grpc.Dial err: %v", err)
+		return
+	}
+	defer conn.Close()
+	client := sample.NewSampleServiceClient(conn)
+	resp, err := client.Search(context.Background(), &sample.SearchRequest{
+		Request: "hello",
+	})
+	if err != nil {
+		t.Errorf("client.Search err: %v", err)
+		return
+	}
+
+	t.Logf("resp: %s", resp.GetResponse())
+}
